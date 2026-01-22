b56d0d7eaba4fadcf4db880997194b7d0a01cdc7
anas-saddique <anassaddique50@email.com>
2026-01-22 17:07:30 +0500

    changes

--- header/main.js
+++ header/main.js
@@ -10,3 +10,12 @@ function openMenu() {
     open = true;
   }
 }
+var typed = new Typed(".text", {
+  strings: ["Frontend Developer", "Youtuber", "Web Developer.."],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+    
